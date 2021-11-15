77a64ca1ab932c504b04838e46f5fe6040544534
rtopham <reedweb@tophamonline.com>
2021-11-15 03:56:25 +0000

    51st commit

--- client/src/components/layout/GoogleAd.js
+++ client/src/components/layout/GoogleAd.js
@@ -46,7 +46,7 @@ const GoogleAd = ({ stripe: { subscription }, setDivClass, show }) => {
           data-ad-client='ca-pub-7288454427087847'
           data-ad-slot='6536318459'
           data-ad-format='auto'
-          data-full-width-responsive='true'
+          /* data-full-width-responsive='true' */
         ></ins>
       </div>
     )
